7f7b63138a37f6c321a571007d77d200316b1fc6
SudaPort <sudaport1@gmail.com>
2021-05-22 10:11:13 +0300

    Update entrypoint.sh

--- docker/core/scripts/entrypoint.sh
+++ docker/core/scripts/entrypoint.sh
@@ -2,86 +2,86 @@
 
 DB_NAME="stellar"
 
-touch core.cfg
-echo "DATABASE=\"postgresql://dbname=$DB_NAME$NODE_NAME user=$PGUSER password=$PGPASSWORD host=$PGHOST\"" >> core.cfg
-echo "" >> core.cfg
-echo "HTTP_PORT=$STELLAR_HTTP_PORT"                                             >> core.cfg
-echo "" >> core.cfg
-echo "PEER_PORT=$STELLAR_PEER_PORT"                                             >> core.cfg
-echo "" >> core.cfg
-echo "PUBLIC_HTTP_PORT=true"                                                    >> core.cfg
-echo "" >> core.cfg
-echo "PREFERRED_PEER_KEYS=[\"$ONE_KEY\", \"$TWO_KEY\"]"                         >> core.cfg
-echo "" >> core.cfg
-# echo "BANK_MASTER_KEY=\"$BANK_MASTER_KEY\""                                   >> core.cfg
-# echo "BANK_COMMISSION_KEY=\"$BANK_COMMISSION_KEY\""                           >> core.cfg
-echo "NETWORK_PASSPHRASE=\"$NETWORK_PASSPHRASE\""                               >> core.cfg
-echo "" >> core.cfg
-echo "NODE_SEED=\"$NODE_SEED\""                                                 >> core.cfg
-echo "" >> core.cfg
-echo "NODE_IS_VALIDATOR=$NODE_IS_VALIDATOR"                                     >> core.cfg
-echo "" >> core.cfg
-echo "CATCHUP_COMPLETE=true"                                                    >> core.cfg
-echo "" >> core.cfg
-echo "FAILURE_SAFETY=0"                                                         >> core.cfg
-echo "" >> core.cfg
-echo "UNSAFE_QUORUM=true"                                                       >> core.cfg
-echo "" >> core.cfg
+touch home/core.cfg
+echo "DATABASE=\"postgresql://dbname=$DB_NAME$NODE_NAME user=$PGUSER password=$PGPASSWORD host=$PGHOST\"" >> home/core.cfg
+echo "" >> home/core.cfg
+echo "HTTP_PORT=$STELLAR_HTTP_PORT"                                             >> home/core.cfg
+echo "" >> home/core.cfg
+echo "PEER_PORT=$STELLAR_PEER_PORT"                                             >> home/core.cfg
+echo "" >> home/core.cfg
+echo "PUBLIC_HTTP_PORT=true"                                                    >> home/core.cfg
+echo "" >> home/core.cfg
+echo "PREFERRED_PEER_KEYS=[\"$ONE_KEY\", \"$TWO_KEY\"]"                         >> home/core.cfg
+echo "" >> home/core.cfg
+# echo "BANK_MASTER_KEY=\"$BANK_MASTER_KEY\""                                   >> home/core.cfg
+# echo "BANK_COMMISSION_KEY=\"$BANK_COMMISSION_KEY\""                           >> home/core.cfg
+echo "NETWORK_PASSPHRASE=\"$NETWORK_PASSPHRASE\""                               >> home/core.cfg
+echo "" >> home/core.cfg
+echo "NODE_SEED=\"$NODE_SEED\""                                                 >> home/core.cfg
+echo "" >> home/core.cfg
+echo "NODE_IS_VALIDATOR=$NODE_IS_VALIDATOR"                                     >> home/core.cfg
+echo "" >> home/core.cfg
+echo "CATCHUP_COMPLETE=true"                                                    >> home/core.cfg
+echo "" >> home/core.cfg
+echo "FAILURE_SAFETY=0"                                                         >> home/core.cfg
+echo "" >> home/core.cfg
+echo "UNSAFE_QUORUM=true"                                                       >> home/core.cfg
+echo "" >> home/core.cfg
 
 if [ ! -z "$PREFERRED_PEERS" ]; then
-    echo "KNOWN_PEERS=$PREFERRED_PEERS"                                         >> core.cfg
-    echo "PREFERRED_PEERS=$PREFERRED_PEERS"                                     >> core.cfg
+    echo "KNOWN_PEERS=$PREFERRED_PEERS"                                         >> home/core.cfg
+    echo "PREFERRED_PEERS=$PREFERRED_PEERS"                                     >> home/core.cfg
 fi
 
-echo "" >> core.cfg
+echo "" >> home/core.cfg
 if [[ $NODE_IS_VALIDATOR == 'true' ]]; then
-    echo "NODE_HOME_DOMAIN=\"${HOME_DOMAIN}\""                                 >> core.cfg
-    echo "" >> core.cfg
+    echo "NODE_HOME_DOMAIN=\"${HOME_DOMAIN}\""                                 >> home/core.cfg
+    echo "" >> home/core.cfg
 fi 
 
-echo "[[HOME_DOMAINS]]"                                                        >> core.cfg
-echo "HOME_DOMAIN=\"$HOME_DOMAIN\""                                            >> core.cfg
-echo "QUALITY=\"MEDIUM\""                                                      >> core.cfg
-echo "" >> core.cfg
+echo "[[HOME_DOMAINS]]"                                                        >> home/core.cfg
+echo "HOME_DOMAIN=\"$HOME_DOMAIN\""                                            >> home/core.cfg
+echo "QUALITY=\"MEDIUM\""                                                      >> home/core.cfg
+echo "" >> home/core.cfg
 
 if [[ $NODE_IS_VALIDATOR != 'true' ]]; then
- echo "[[VALIDATORS]]"                                                         >> core.cfg
- echo "NAME=\"validatornode\""                                                 >> core.cfg
-#  echo "QUALITY=\"MEDIUM\""                                                   >> core.cfg
- echo "HOME_DOMAIN=\"$HOME_DOMAIN\""                                           >> core.cfg
- echo "PUBLIC_KEY=\"${VALIDATORS}\""                                           >> core.cfg
-#  echo "ADDRESS=\"${HOME_DOMAIN}:11645\""                                     >> core.cfg
+ echo "[[VALIDATORS]]"                                                         >> home/core.cfg
+ echo "NAME=\"validatornode\""                                                 >> home/core.cfg
+#  echo "QUALITY=\"MEDIUM\""                                                   >> home/core.cfg
+ echo "HOME_DOMAIN=\"$HOME_DOMAIN\""                                           >> home/core.cfg
+ echo "PUBLIC_KEY=\"${VALIDATORS}\""                                           >> home/core.cfg
+#  echo "ADDRESS=\"${HOME_DOMAIN}:11645\""                                     >> home/core.cfg
 fi
 chmod u+x /scripts/riakget.sh
 chmod u+x /scripts/riakput.sh
-echo "" >> core.cfg
-echo "[HISTORY.riak]"                                                                          >> core.cfg
-echo "get=\"./scripts/riakget.sh $RIAK_HOST $RIAK_BUCKET {0} {1} $RIAK_USER $RIAK_PASS\""      >> core.cfg
-echo "put=\"./scripts/riakput.sh $RIAK_HOST $RIAK_BUCKET {0} {1} $RIAK_USER $RIAK_PASS\""      >> core.cfg
-echo "mkdir=\"mkdir -p {0}\""                                                                  >> core.cfg
-echo "" >> core.cfg
-# echo "[HISTORY.azure]"                                                                      >> core.cfg
-# echo "get=\"***\""                                                                          >> core.cfg
-# echo "put=\"***\""                                                                          >> core.cfg
+echo "" >> home/core.cfg
+echo "[HISTORY.riak]"                                                                          >> home/core.cfg
+echo "get=\"./scripts/riakget.sh $RIAK_HOST $RIAK_BUCKET {0} {1} $RIAK_USER $RIAK_PASS\""      >> home/core.cfg
+echo "put=\"./scripts/riakput.sh $RIAK_HOST $RIAK_BUCKET {0} {1} $RIAK_USER $RIAK_PASS\""      >> home/core.cfg
+echo "mkdir=\"mkdir -p {0}\""                                                                  >> home/core.cfg
+echo "" >> home/core.cfg
+# echo "[HISTORY.azure]"                                                                      >> home/core.cfg
+# echo "get=\"***\""                                                                          >> home/core.cfg
+# echo "put=\"***\""                                                                          >> home/core.cfg
 
-#echo "[HISTORY.local]"                                                                       >> core.cfg
-#echo "get=\"cp /tmp/stellar-core/history/vs/{0} {1}\""                                       >> core.cfg
-#echo "put=\"cp {0} /tmp/stellar-core/history/vs/{1}\""                                       >> core.cfg
-#echo "mkdir=\"mkdir -p /tmp/stellar-core/history/vs/{0}\""                                   >> core.cfg
-src/stellar-core --conf core.cfg
+#echo "[HISTORY.local]"                                                                       >> home/core.cfg
+#echo "get=\"cp /tmp/stellar-core/history/vs/{0} {1}\""                                       >> home/core.cfg
+#echo "put=\"cp {0} /tmp/stellar-core/history/vs/{1}\""                                       >> home/core.cfg
+#echo "mkdir=\"mkdir -p /tmp/stellar-core/history/vs/{0}\""                                   >> home/core.cfg
+src/stellar-core --conf home/core.cfg
 # Comment out if not new network
 if [[ $NODE_NAME == 'core' ]]; then
-        src/stellar-core --conf core.cfg
+        src/stellar-core --conf home/core.cfg
         src/stellar-core new-hist riak
         # src/stellar-core new-hist azure
         src/stellar-core new-db
 
 elif [[ $NODE_NAME == 'fee' ]]; then
-        src/stellar-core --conf core.cfg
+        src/stellar-core --conf home/core.cfg
         src/stellar-core new-db
 
 elif [[ $NODE_NAME == 'validator' ]]; then
-        src/stellar-core --conf core.cfg
+        src/stellar-core --conf home/core.cfg
         src/stellar-core new-db
 else 
        echo "Unknown node role..."
@@ -94,8 +94,8 @@ fi
 # if [[ $TABLE_EXISTS == 0 ]]; then
 #     echo "Initializing Dabatase"
 #     # --newhist flag should run prior to new-db!!! 
-#     #src/stellar-core --conf core.cfg --newhist local
-#  src/stellar-core --conf core.cfg
+#     #src/stellar-core --conf home/core.cfg --newhist local
+#  src/stellar-core --conf home/core.cfg
 #     if [[ $NODE_IS_VALIDATOR == 'true' ]]; then 
 #         src/stellar-core new-hist riak
 #         # src/stellar-core new-hist azure
@@ -108,4 +108,4 @@ fi
 #     exit
 # fi
 
-src/stellar-core --conf core.cfg
+src/stellar-core --conf home/core.cfg
