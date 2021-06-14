817d79c59f81e4448b29cfd3ef53f7454504ea6b
SudaPort <sudaport1@gmail.com>
2021-06-14 12:56:25 +0300

    Update entrypoint.sh

--- docker/horizon/entrypoint.sh
+++ docker/horizon/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-bin/horizon --db-url
-
-bin/horizon
+bin/horizon --stellar-core-url postgres://root:root@188.0.0.24:5432/horizon?sslmode=disable
+bin/horizon --stellar-core-db-url postgres://root:root@188.0.0.24:5432/stellarcore?sslmode=disable
+bin/horizon --db-url http://188.0.0.22:11625
+bin/horizon horizon db migrate up
+bin/horizon horizon serve
